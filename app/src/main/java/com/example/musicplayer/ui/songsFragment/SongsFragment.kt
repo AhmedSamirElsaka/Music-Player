e453ef32d394242d19adb0faabf87de178d551f3
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-06-22 13:55:04 +0300

    finish song and artist and album design screens

---
+++ app/src/main/java/com/example/musicplayer/ui/songsFragment/SongsFragment.kt
@@ -0,0 +1,32 @@
+package com.example.musicplayer.ui.songsFragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.musicplayer.R
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [SongsFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class SongsFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_songs, container, false)
+    }
+
+}
