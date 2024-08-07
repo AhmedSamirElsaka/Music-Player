88f067e28c28209ea651e86bc0fbf188df378156
Ahmed Samir Mohamed <ahmedsamer65491@gmail.com>
2024-08-07 16:16:16 +0300

    finish app

---
+++ app/src/main/java/com/example/musicplayer/ui/SplashScreenActivity.kt
@@ -0,0 +1,39 @@
+package com.example.musicplayer.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.musicplayer.MainActivity
+import com.example.musicplayer.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        setContentView(R.layout.activity_splash_screen)
+        supportActionBar?.hide()
+        GlobalScope.launch(Dispatchers.Main) {
+            delay(3000)
+            nextActivity()
+        }
+    }
+    private fun nextActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
